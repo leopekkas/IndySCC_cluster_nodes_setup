89f3bed43222996f23c4a71dc18c058a5e89cb36
leopekkas <leopekkas@gmail.com>
2022-09-26 19:05:38 +0300

    Add 05 working

--- slurm_setup/04_rpms.sh
+++ slurm_setup/04_rpms.sh
@@ -2,9 +2,11 @@
 mkdir -p /home/cc/slurm_setup/rpms
 cd /home/cc/slurm_setup/rpms
 
-yum install perl openssl openssl-devel pam-devel rpm-build numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
+yum install perl openssl openssl-devel pam-devel rpm-build numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad mysql-devel -y
 wget https://download.schedmd.com/slurm/slurm-21.08-latest.tar.bz2
 
 rpmbuild -ta slurm-21.08-latest.tar.bz2
 
 yum --nogpgcheck localinstall slurm-* -y
+
+echo "Please create the mysql database"
--- slurm_setup/05_slurm.sh
+++ slurm_setup/05_slurm.sh
@@ -1,4 +1,17 @@
 # Every node should have the same copy of slurm.conf. If you change it, be sure to copy it around
+echo "AuthType=auth/munge
+  DbdAddr=192.168.1.250
+  DbdHost=master0
+  SlurmUser=slurm
+  DebugLevel=4
+  LogFile=/var/log/slurm/slurmdbd.log
+  PidFile=/var/run/slurmdbd.pid
+  StorageType=accounting_storage/mysql
+  StorageHost=master0
+  StoragePass=some_pass
+  StorageUser=slurm
+  StorageLoc=slurm_acct_db" >> /etc/slurm/slurmdbd.conf
+
 cp /home/cc/slurm_setup/slurm.conf /etc/slurm/
 
 cp /home/cc/slurm_setup/cgroup.conf /etc/slurm/
