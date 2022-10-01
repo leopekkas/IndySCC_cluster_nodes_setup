6cced7db86ecfe75eef7d5b589b17d618b9695f4
Leopekka Saraste <45951298+leopekkas@users.noreply.github.com>
2022-10-01 17:40:43 +0300

    Fix paths

--- compute_node_setup/slurm_setup/04_slurm_node.sh
+++ compute_node_setup/slurm_setup/04_slurm_node.sh
@@ -1,20 +1,20 @@
 # Every node should have the same copy of slurm.conf. If you change it, be sure to copy it around
 echo "AuthType=auth/munge
   DbdAddr=10.20.30.52
-  DbdHost=login-node
+  DbdHost=revontuli-hpl-instance
   SlurmUser=slurm
   DebugLevel=4
   LogFile=/var/log/slurm/slurmdbd.log
   PidFile=/var/run/slurmdbd.pid
   StorageType=accounting_storage/mysql
-  StorageHost=login-node
+  StorageHost=revontuli-hpl-instance
   StoragePass=some_pass
   StorageUser=slurm
   StorageLoc=slurm_acct_db" >> /etc/slurm/slurmdbd.conf
 
-cp /home/cc/slurm_setup/slurm.conf /etc/slurm/
+cp /tmp/slurm_setup/slurm.conf /etc/slurm/
 
-cp /home/cc/slurm_setup/cgroup.conf /etc/slurm/
+cp /tmp/slurm_setup/cgroup.conf /etc/slurm/
 
 mkdir /var/spool/slurmd
 chown slurm: /var/spool/slurmd
