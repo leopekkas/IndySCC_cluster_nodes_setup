957594c75120b8f6b4b4c6342f29542a414edf76
Leopekka Saraste <45951298+leopekkas@users.noreply.github.com>
2022-10-01 17:28:51 +0300

    Fix relative paths to absolute

--- compute_node_setup/initial_setup/setup.sh
+++ compute_node_setup/initial_setup/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy our etc hosts over. Make sure you grab the versino you just made.
-cp hosts /etc/
+cp /tmp/initial_setup/hosts /etc/
 
 # Make a list of packages you may need. I like having vim... the others are bare minimum. 
 # I recommend adding others as you find you need here so if you need to reinstall you're not losing anything.
@@ -12,6 +12,8 @@ yum install tk tcl tcsh -y
 mkdir -p /apps
 
 # /etc/fstab tells the OS what to mount
+echo "LABEL=cloudimg-rootfs / ext4 defaults 0 1" > /etc/fstab
+echo "LABEL=MKFS_ESP /boot/efi vfat defaults 0 2" >> /etc/fstab
 echo "revontuli-hpl-instance:/home /home nfs defaults 0 0" >> /etc/fstab
 echo "revontuli-hpl-instance:/apps /apps nfs defaults 0 0" >> /etc/fstab
 
@@ -19,4 +21,4 @@ mount -a
 
 # Need to set a few things here to make slurm work
 # Also having stack size set to unlimited is important. Super super common to run into problems without it.
-cp limits.conf /etc/security/
+cp /tmp/initial_setup/limits.conf /etc/security/
