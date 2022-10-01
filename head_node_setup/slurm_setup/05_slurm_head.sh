0fde999bbbc164e151fb6bc531df2afc83c02daa
Leopekka Saraste <45951298+leopekkas@users.noreply.github.com>
2022-10-01 17:42:05 +0300

    Fix naming

--- head_node_setup/slurm_setup/05_slurm_head.sh
+++ head_node_setup/slurm_setup/05_slurm_head.sh
@@ -1,13 +1,13 @@
 # Every node should have the same copy of slurm.conf. If you change it, be sure to copy it around
 echo "AuthType=auth/munge
   DbdAddr=10.20.30.52
-  DbdHost=login-node
+  DbdHost=revontuli-hpl-instance
   SlurmUser=slurm
   DebugLevel=4
   LogFile=/var/log/slurm/slurmdbd.log
   PidFile=/var/run/slurmdbd.pid
   StorageType=accounting_storage/mysql
-  StorageHost=login-node
+  StorageHost=revontuli-hpl-instance
   StoragePass=some_pass
   StorageUser=slurm
   StorageLoc=slurm_acct_db" >> /etc/slurm/slurmdbd.conf
